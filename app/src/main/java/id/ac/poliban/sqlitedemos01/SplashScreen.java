4d3c23278cafdf9777f3e3ce52216293733e0b31
unknown <radiansyah10>
2020-01-14 21:53:35 -0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/sqlitedemos01/SplashScreen.java
@@ -0,0 +1,34 @@
+package id.ac.poliban.sqlitedemos01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import id.ac.poliban.mi.sqlitedemos01.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ImageView logo = findViewById(R.id.logo);
+
+        Glide.with(this)
+                .load(Uri.parse("https://5.imimg.com/data5/JM/TK/HQ/SELLER58228872/sambhaji-maharaj-sitting-fiber-statue-500x500.jpg"))
+                .into(logo);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }, 3000);
+    }
+
+}
